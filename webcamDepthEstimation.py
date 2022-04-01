23a734c8e786b93a548461b9d159bc1db41467cc
stephan <stephanjacobs@outlook.com>
2022-04-01 22:10:04 +1100

    removed overlayed image from display

--- webcamDepthEstimation.py
+++ webcamDepthEstimation.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 from MidasDepthEstimation.midasDepthEstimator import midasDepthEstimator
 
@@ -6,28 +7,31 @@ from MidasDepthEstimation.midasDepthEstimator import midasDepthEstimator
 depthEstimator = midasDepthEstimator()
 
 # Initialize webcam
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+camera = cv2.VideoCapture(0)
 cv2.namedWindow("Depth Image", cv2.WINDOW_NORMAL) 	
 
 while True:
+    time.sleep(2)
+    # Read frame from the webcam
+    ret, img = camera.read()
 
-	# Read frame from the webcam
-	ret, img = camera.read()	
+    # Estimate depth
+    colorDepth = depthEstimator.estimateDepth(img)
+    
+    time.sleep(2)
+    # Add the depth image over the color image:
+    #combinedImg = cv2.addWeighted(img,0.7,colorDepth,0.6,0)
 
-	# Estimate depth
-	colorDepth = depthEstimator.estimateDepth(img)
+    # Join the input image, the estiamted depth and the combined image
+    #img_out = np.hstack((img, colorDepth, combinedImg))
+    img_out = np.hstack((img, colorDepth))
 
-	# Add the depth image over the color image:
-	combinedImg = cv2.addWeighted(img,0.7,colorDepth,0.6,0)
+    cv2.imshow("Depth Image", img_out)
+    #cv2.imshow("Image", img)
 
-	# Join the input image, the estiamted depth and the combined image
-	img_out = np.hstack((img, colorDepth, combinedImg))
-
-	cv2.imshow("Depth Image", img_out)
-
-	# Press key q to stop
-	if cv2.waitKey(1) == ord('q'):
-		break
+    # Press key q to stop
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 camera.release()
 cv2.destroyAllWindows()
